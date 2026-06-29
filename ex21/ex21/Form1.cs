2670f53b17694d319dd8f8044f43b8d7a58ec8ca
Celine Santos Pereira <celinesantospereira7@gmail.com>
2026-06-29 20:30:53 -0300

    ex21

---
+++ ex21/ex21/Form1.cs
@@ -0,0 +1,23 @@
+namespace ex21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox1.Text);
+            string resultado = "";
+
+            for (int i = 0; i <= 10; i++)
+            {
+                resultado += $"{n} x {i} = {n * i}\n";
+            }
+
+            MessageBox.Show(resultado);
+        }
+    }
+}
