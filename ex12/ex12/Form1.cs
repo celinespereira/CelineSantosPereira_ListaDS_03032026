3d92b7b41719ff49dd7fddd4578e7e4b36715f07
Celine Santos Pereira <celinesantospereira7@gmail.com>
2026-06-29 20:09:10 -0300

    ex12

---
+++ ex12/ex12/Form1.cs
@@ -0,0 +1,27 @@
+namespace ex12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string numeros = "";
+
+            for (int i = 1; i <= 5; i++)
+            {
+                numeros = numeros + i; 
+
+                if (i < 5) 
+                {
+                    numeros = numeros + "-"; 
+                }
+            }
+
+            textBox1.Text = numeros;
+        }
+    }
+}
