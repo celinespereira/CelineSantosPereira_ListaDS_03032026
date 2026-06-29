55ef105d0fdc80dbe90ecb94ceb74dae9109804e
Celine Santos Pereira <celinesantospereira7@gmail.com>
2026-06-29 20:20:22 -0300

    ex17

---
+++ ex17/ex17/Form1.cs
@@ -0,0 +1,15 @@
+namespace ex17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Minha idade é: 16";
+        }
+    }
+}
