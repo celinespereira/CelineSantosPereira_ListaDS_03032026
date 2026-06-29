4d2c6b319a73bc8c11d344778c3b85892c73839f
Celine Santos Pereira <celinesantospereira7@gmail.com>
2026-06-29 19:58:06 -0300

    ex8

---
+++ ex8/ex8/Form1.cs
@@ -0,0 +1,15 @@
+namespace ex8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Domingo\nSegunda\nTerça\nQuarta\nQuinta\nSexta\nSábado";
+        }
+    }
+}
