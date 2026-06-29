4bb1c1f6500385b18a18a4ee36c42f3c341b90b3
Celine Santos Pereira <celinesantospereira7@gmail.com>
2026-06-29 20:01:03 -0300

    ex9

---
+++ ex9/ex9/Form1.cs
@@ -0,0 +1,17 @@
+namespace ex9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "3 x 0 = 0\n3 x 1 = 3\n3 x 2 = 6\n3 x 3 = 9\n" +
+                           "3 x 4 = 12\n3 x 5 = 15\n3 x 6 = 18\n3 x 7 = 21\n" +
+                           "3 x 8 = 24\n3 x 9 = 27\n3 x 10 = 30";
+        }
+    }
+}
