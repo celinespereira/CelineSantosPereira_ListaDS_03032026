575388f9f7376358aacbc0a607f0280c83f16db2
Celine Santos Pereira <celinesantospereira7@gmail.com>
2026-06-29 20:12:51 -0300

    ex13

---
+++ ex13/ex13/Form1.cs
@@ -0,0 +1,16 @@
+namespace ex13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Janeiro\nFevereiro\nMarço\nAbril\nMaio\nJunho\n" +
+                         "Julho\nAgosto\nSetembro\nOutubro\nNovembro\nDezembro";
+        }
+    }
+}
