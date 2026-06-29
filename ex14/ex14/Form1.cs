95c5deda326645f9a926a4a8e96b11530256e7c7
Celine Santos Pereira <celinesantospereira7@gmail.com>
2026-06-29 20:13:45 -0300

    ex14

---
+++ ex14/ex14/Form1.cs
@@ -0,0 +1,15 @@
+namespace ex14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Domingo\nSegunda\nTerça\nQuarta\nQuinta\nSexta\nSábado";
+        }
+    }
+}
